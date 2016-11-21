2e483b9fe6d77e6dc7f8b81fc2adbcca265053da
Jordi Borràs i Vivó <xordiet@users.noreply.github.com>
2016-11-21 16:19:12 +0100

    Initial Commit

---
+++ hamburgueses-mon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hamburgueses-mon
+//
+//  Created by Jordi Borràs i Vivó on 21/11/16.
+//  Copyright © 2016 Jordi Borràs i Vivó. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
